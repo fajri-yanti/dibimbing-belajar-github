1df884d6b9f8827f6d7dd6fe4d455d09817161d7
fajri-yanti <fajriyantifaj@gmail.com>
2024-11-26 00:19:31 +0700

    add script.py

---
+++ script.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+path = r'D:\dibimbing DE\git-assignment\dibimbing-belajar-github\username.csv'
+
+def read_csv(path, sep=','):
+    return pd.read_csv(path, sep=sep)
+
+df = read_csv(path, sep=';')
+print(df)
